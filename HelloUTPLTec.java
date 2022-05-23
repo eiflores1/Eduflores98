ef2e998a1244fa0708c00b52569a42005e4ff5f9
eiflores1 <eduflores98@outlook.es>
2022-05-23 18:38:55 +0000

    agregar archivo

---
+++ HelloUTPLTec.java
@@ -0,0 +1,8 @@
+/**
+ * Class Untitled
+ */
+public class HelloUTPLTec {
+	public static void main(String[] args) {
+    	System.out.println("HelloUTPLTec from AWS Cloud9! into HelloUTPLTec");
+	}
+}
